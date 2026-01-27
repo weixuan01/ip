297d29b799c75a6427c3a5c202594839642014ab
Poon Wei Xuan <poonweixuan@gmail.com>
2026-01-27 19:37:45 +0800

    Make repeated line string a constant

--- src/main/java/Dill.java
+++ src/main/java/Dill.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class Dill {
     public static final String CHATBOT_NAME = "Dill";
+    private static final String LINE = "    ____________________________________________________________";
 
     public static void main(String[] args) {
-        System.out.println("    ____________________________________________________________");
+        System.out.print(LINE);
         System.out.println("    Hello! I'm " + CHATBOT_NAME + ".");
         System.out.println("    What can I do for you?");
-        System.out.println("    ____________________________________________________________");
+        System.out.print(LINE);
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
-                System.out.println("    ____________________________________________________________");
+                System.out.print(LINE);
                 System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________");
+                System.out.print(LINE);
                 break;
             }
-            System.out.println("    ____________________________________________________________");
+            System.out.print(LINE);
             System.out.println("    " + userInput);
-            System.out.println("    ____________________________________________________________");
+            System.out.print(LINE);
         }
     }
 }
