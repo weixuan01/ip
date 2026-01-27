3d5d76f08db9458600a1b1ed13047efbb9ec8480
Poon Wei Xuan <poonweixuan@gmail.com>
2026-01-27 17:53:17 +0800

    Implement basic greeting and exit

--- src/main/java/Dill.java
+++ src/main/java/Dill.java
@@ -2,5 +2,11 @@ public class Dill {
     public static final String CHATBOT_NAME = "Dill";
 
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + CHATBOT_NAME + ".");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
