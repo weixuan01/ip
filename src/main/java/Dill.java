e2e7e1c028e51b016bb1172224773747a1087b8d
Poon Wei Xuan <poonweixuan@gmail.com>
2026-01-31 22:05:11 +0800

    Implement error handling and add help command

--- src/main/java/Dill.java
+++ src/main/java/Dill.java
@@ -22,15 +22,26 @@ public class Dill {
 
     private static void printTaskList() {
         System.out.println(LINE);
-        System.out.println("    Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println("    " + (i + 1) + "." + tasks.get(i));
+        if (tasks.isEmpty()) {
+            System.out.println("    There are no tasks in the list!");
+        } else {
+            System.out.println("    Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println("    " + (i + 1) + "." + tasks.get(i));
+            }
         }
         System.out.println(LINE);
     }
 
-    private static void markTask(String userInput) {
+    // also throws NumberFormatException if argument cannot be parsed to int
+    private static void markTask(String userInput) throws InvalidCommandException {
+        if (!userInput.startsWith("mark ") || userInput.equals("mark ")) {
+            throw new InvalidCommandException("Please specify an entry to mark.");
+        }
         int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+        if (taskIndex > tasks.size() - 1 || taskIndex < 0) {
+            throw new InvalidCommandException("Cannot mark an entry that is not in the list!");
+        }
         tasks.get(taskIndex).markDone();
         System.out.println(LINE);
         System.out.println("    Nice! I've marked this task as done:");
@@ -38,8 +49,14 @@ public class Dill {
         System.out.println(LINE);
     }
 
-    private static void unmarkTask(String userInput) {
+    private static void unmarkTask(String userInput) throws InvalidCommandException {
+        if (!userInput.startsWith("unmark ") || userInput.equals("unmark ")) {
+            throw new InvalidCommandException("Please specify an entry to unmark.");
+        }
         int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+        if (taskIndex > tasks.size() - 1 || taskIndex < 0) {
+            throw new InvalidCommandException("Cannot unmark an entry that is not in the list!");
+        }
         tasks.get(taskIndex).markUndone();
         System.out.println(LINE);
         System.out.println("    OK, I've marked this task as not done yet:");
@@ -47,20 +64,44 @@ public class Dill {
         System.out.println(LINE);
     }
 
-    private static void addToDo(String userInput) {
+    private static void addToDo(String userInput) throws InvalidCommandException {
+        if (!userInput.startsWith("todo ") || userInput.equals("todo ")) {
+            throw new InvalidCommandException("Please specify todo task name.");
+        }
         String taskName = userInput.substring(5);
         tasks.add(new ToDo(taskName));
         printTaskAdded();
     }
 
-    private static void addDeadline(String userInput) {
-        String taskName = userInput.substring(9, userInput.indexOf("/by") - 1);
-        String deadline = userInput.substring(userInput.indexOf("/by") + 4);
+    private static void addDeadline(String userInput) throws InvalidCommandException{
+        int byIndex = userInput.indexOf("/by ");
+        if (!userInput.startsWith("deadline ") || userInput.equals("deadline ") || byIndex == 9) {
+            throw new InvalidCommandException("Please specify deadline task name");
+        }
+        if (byIndex < 0) {
+            throw new InvalidCommandException("Please specify a deadline.");
+        }
+        String taskName = userInput.substring(9, byIndex - 1);
+        String deadline = userInput.substring(byIndex + 4);
         tasks.add(new Deadline(taskName, deadline));
         printTaskAdded();
     }
 
-    private static void addEvent(String userInput) {
+    private static void addEvent(String userInput) throws InvalidCommandException {
+        int startIndex = userInput.indexOf("/start ");
+        int endIndex = userInput.indexOf("/end ");
+        if (!userInput.startsWith("event ") || userInput.equals("event ") || startIndex == 6 || endIndex == 6) {
+            throw new InvalidCommandException("Please specify event task name.");
+        }
+        if (startIndex < 0) {
+            throw new InvalidCommandException("Please specify a start time.");
+        }
+        if (endIndex < 0) {
+            throw new InvalidCommandException("Please specify an end time.");
+        }
+        if (endIndex < startIndex) {
+            throw new InvalidCommandException("Please specify start time followed by end time, in that order.");
+        }
         String taskName = userInput.substring(6, userInput.indexOf("/start") - 1);
         String startTime = userInput.substring(userInput.indexOf("/start") + 7, userInput.indexOf("/end") - 1);
         String endTime = userInput.substring(userInput.indexOf("/end") + 5);
@@ -76,29 +117,52 @@ public class Dill {
         System.out.println(LINE);
     }
 
+    private static void printHelp() {
+        System.out.println(LINE);
+        System.out.println("    Here are the available commands:");
+        System.out.println("        list: displays the tasks in your list.");
+        System.out.println("        todo <task_name>: adds a todo task.");
+        System.out.println("        deadline <task_name> /by <deadline>: adds a deadline task.");
+        System.out.println("        event <task_name> /start <start_time> /end <end_time>: adds an event task.");
+        System.out.println("        mark <entry_number>: marks task as done.");
+        System.out.println("        unmark <entry_number>: marks task as undone.");
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
         printGreeting();
-
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String userInput = scanner.nextLine();
-            if (userInput.equals("bye")) {
-                printExit();
-                break;
-            } else if (userInput.equals("list")) {
-                printTaskList();
-            } else if (userInput.startsWith("mark ")) {
-                markTask(userInput);
-            } else if (userInput.startsWith("unmark ")) {
-                unmarkTask(userInput);
-            } else if (userInput.startsWith("todo ")) {
-                addToDo(userInput);
-            } else if (userInput.startsWith("deadline ")) {
-                addDeadline(userInput);
-            } else if (userInput.startsWith("event ")) {
-                addEvent(userInput);
-            } else {
-                System.out.println("Invalid input: task must be todo, deadline, or event.");
+            try {
+                String userInput = scanner.nextLine();
+                if (userInput.equals("bye")) {
+                    printExit();
+                    break;
+                } else if (userInput.equals("list")) {
+                    printTaskList();
+                } else if (userInput.startsWith("mark")) {
+                    markTask(userInput);
+                } else if (userInput.startsWith("unmark")) {
+                    unmarkTask(userInput);
+                } else if (userInput.startsWith("todo")) {
+                    addToDo(userInput);
+                } else if (userInput.startsWith("deadline")) {
+                    addDeadline(userInput);
+                } else if (userInput.startsWith("event")) {
+                    addEvent(userInput);
+                } else if (userInput.equals("help")) {
+                    printHelp();
+                } else {
+                    throw new InvalidCommandException("I'm not quite sure what you meant.\n    Type \"help\" if you wish to view a list of available commands.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println(LINE);
+                System.out.println("    Entry must be an integer!");
+                System.out.println(LINE);
+            } catch (InvalidCommandException e) {
+                System.out.println(LINE);
+                System.out.println("    " + e.getMessage());
+                System.out.println(LINE);
             }
         }
     }
