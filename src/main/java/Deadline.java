7912981f3a1b348bf55bbb69f1a6ec622a46e531
Poon Wei Xuan <poonweixuan@gmail.com>
2026-02-02 22:14:14 +0800

    Implement toFileString method for saving tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -9,4 +9,8 @@ public class Deadline extends Task {
     public String toString() {
         return "[D]" + super.toString() + " (by: " + date + ")";
     }
+
+    public String toFileString() {
+        return "D | " + super.toFileString() + " | " + date;
+    }
 }
