014f5f950f91b46cbe1aa864dddd5446e9b8c42b
Poon Wei Xuan <poonweixuan@gmail.com>
2026-02-08 23:55:06 +0800

    Add JavaDoc for exceptions

--- src/main/java/dill/exception/StorageException.java
+++ src/main/java/dill/exception/StorageException.java
@@ -1,5 +1,9 @@
 package dill.exception;
 
+/**
+ * Represents an error that occurs during data storage operations.
+ * Typically due to an invalid file path or issues reading/writing from/to the data storage file.
+ */
 public class StorageException extends DillException {
     public StorageException(String message) {
         super(message);
