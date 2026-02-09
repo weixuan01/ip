fb67cfe77730a6d09ad9384e240fbc26f8de6c8f
Poon Wei Xuan <poonweixuan@gmail.com>
2026-02-09 15:59:20 +0800

    Add missing delete command in help message

--- src/main/java/dill/command/HelpCommand.java
+++ src/main/java/dill/command/HelpCommand.java
@@ -16,6 +16,7 @@ public class HelpCommand extends Command {
             + "        event <task_name> /start <yyyy-mm-dd> /end <yyyy-mm-dd>: adds an event task.\n"
             + "        mark <entry_number>: marks a task as done.\n"
             + "        unmark <entry_number>: marks a task as undone.\n"
+            + "        delete <entry_number>: removes a task from the list.\n"
             + "        find <keyword>: displays tasks matching the specified keyword.\n"
             + "        cheer: displays a random motivational message.";
 
