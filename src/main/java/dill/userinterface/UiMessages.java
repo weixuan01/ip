23d13eb462c4a57a391bc175f3950213bf39d6da
Poon Wei Xuan <poonweixuan@gmail.com>
2026-02-16 16:51:31 +0800

    Update help message

--- src/main/java/dill/userinterface/UiMessages.java
+++ src/main/java/dill/userinterface/UiMessages.java
@@ -6,17 +6,29 @@ public class UiMessages {
     private static final String LOAD_TASKS_SUCCESS_EMPTY = "I could not find any saved tasks. Let's start fresh!";
     private static final String LOAD_TASKS_ERROR = "%s\nThis session will not be saved.";
     private static final String EXIT_MESSAGE = "Bye. Hope to see you again soon!";
-    private static final String HELP_MESSAGE = "Here are the available commands:\n"
-            + "list\n"
-            + "todo <task-name>\n"
-            + "deadline <task-name> /by <yyyy-mm-dd>\n"
-            + "event <task-name> /start <yyyy-mm-dd> /end <yyyy-mm-dd>\n"
-            + "mark <entry-number>\n"
-            + "unmark <entry-number>\n"
-            + "delete <entry-number>\n"
-            + "find <keyword>\n"
-            + "cheer\n"
-            + "bye";
+    private static final String HELP_MESSAGE =
+            """
+            Here are the available commands:"
+            1. list
+               Displays the current tasks in the list.
+            2. todo <task-name>
+               Adds a to-do task to the list.
+            3. deadline <task-name> /by <yyyy-mm-dd>
+               Adds a deadline task to your list.
+            4. event <task-name> /start <yyyy-mm-dd> /end <yyyy-mm-dd>
+               Adds an event task to the list.
+            5. mark <entry-number>
+               Marks the task at specified entry as complete.
+            6. unmark <entry-number>
+               Marks the task at specified entry as incomplete.
+            7. delete <entry-number>
+               Removes the task at specified entry from the list.
+            8. find <keyword>
+               Displays all tasks matching the specified keyword.
+            9. cheer
+               Displays a random motivational message.
+            10. bye
+               Terminates the current session.""";
 
     public static String getGreeting(String loadMessage) {
         return String.format(GREETING, loadMessage);
