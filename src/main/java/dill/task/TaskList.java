040e38141bfae7ecc1c0eabc8ab0a954e4d1a813
Poon Wei Xuan <poonweixuan@gmail.com>
2026-02-18 23:50:43 +0800

    Add more assertions to core components
    Integrate assertions into Dill, Deadline, Event, and Tasklist
    classes to document important assumptions and verify invariants.
    
    - Add null checks in Dill constructor during initialization.
    - Verify LocalDate argument is not null for isOccuringOn() in deadline
    and Event classes.
    - Add null checks in TaskList constructor during initialization
    - Check input task is not null for addTask(), markTask(), unmarkTask()
    and deleteTask() in Task class.
    
    These assertions ensure that errors are caught early during
    development when running with the -ea flag.

--- src/main/java/dill/task/TaskList.java
+++ src/main/java/dill/task/TaskList.java
@@ -41,6 +41,7 @@ public class TaskList {
      * @param tasks The list of tasks to be managed.
      */
     public TaskList(List<Task> tasks) {
+        assert tasks != null : "input list should not be null";
         this.tasks = tasks;
     }
 
@@ -78,6 +79,7 @@ public class TaskList {
      * @param task The task to be added.
      */
     public void addTask(Task task) {
+        assert task != null : "task should never be null";
         tasks.add(task);
     }
 
@@ -87,6 +89,7 @@ public class TaskList {
      * @param taskIndex The index of the task in the list.
      */
     public void markTask(int taskIndex) {
+        assert (taskIndex < tasks.size() && taskIndex >= 0) : "Index should be within valid range";
         tasks.get(taskIndex).setDone();
     }
 
@@ -96,6 +99,7 @@ public class TaskList {
      * @param taskIndex The index of the task in the list.
      */
     public void unmarkTask(int taskIndex) {
+        assert (taskIndex < tasks.size() && taskIndex >= 0) : "Index should be within valid range";
         tasks.get(taskIndex).setUndone();
     }
 
@@ -106,6 +110,7 @@ public class TaskList {
      * @return The removed task.
      */
     public Task deleteTask(int taskIndex) {
+        assert (taskIndex < tasks.size() && taskIndex >= 0) : "Index should be within valid range";
         return tasks.remove(taskIndex);
     }
 
@@ -130,7 +135,7 @@ public class TaskList {
         List<MatchingTask> matchingTasks = new ArrayList<>();
         for (int i = 0; i < tasks.size(); i++) {
             Task t = tasks.get(i);
-            if (t.isOccuringOn(date)) {
+            if (t.isOccurringOn(date)) {
                 matchingTasks.add(new MatchingTask(t, i));
             }
         }
